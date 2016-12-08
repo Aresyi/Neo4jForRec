66053b246edf754e300cf90c58bff030aa22a736
yideun <dejun.yi@9tong.com>
2016-12-08 16:45:12 +0800

    init

---
+++ src/main/java/com/ydj/neo4j/Neo4jForRec/App.java
@@ -0,0 +1,13 @@
+package com.ydj.neo4j.Neo4jForRec;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
